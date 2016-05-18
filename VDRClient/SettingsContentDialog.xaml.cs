96cb394a3c0e578f619ae3aba517d4c547da863c
nanohcv <karl_wintermann@gmx.de>
2016-05-18 21:30:14 +0200

    Missing Files added

---
+++ VDRClient/SettingsContentDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Inhaltsdialog" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace VDRClient
+{
+    public sealed partial class SettingsContentDialog : ContentDialog
+    {
+        public SettingsContentDialog(VDR.VDRList settings)
+        {
+            this.InitializeComponent();
+            SettingsListBox.ItemsSource = settings;
+            SettingsListBox.SelectedItem = settings.LastUsedSettings;
+        }
+
+        public VDR.Settings SelectedSetting { get; private set; }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            SelectedSetting = (VDR.Settings)SettingsListBox.SelectedItem;
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
